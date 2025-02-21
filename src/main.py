20bc8ac2608d6897cfb4db56a9374ff56c4c6645
selena-mei <mei.selena923@gmail.com>
2025-02-21 16:29:17 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-## visual 1: scatter
+# all imports
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import altair as alt
+import pandas as pd
+import folium
+from folium.plugins import HeatMap
+
 load_listing = load_data('Listings.csv')
 listing_data = clean_listing_data(load_listing)
 alt.data_transformers.disable_max_rows()
-scatter(listing_data,'review_scores_rating','price')
 
+## visual 1: scatter
+scatter(listing_data,'review_scores_rating','price')
+## visual 2: geospatial heatmap
+geospatial_hm(listing_data,'latitude','longitude','price')
