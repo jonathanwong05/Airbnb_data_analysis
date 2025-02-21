fa5698b01f5447cf9570af6a0bf1733f8dbd38b8
selena-mei <mei.selena923@gmail.com>
2025-02-21 02:32:34 -0500

    Update visualizations.py

--- src/visualizations.py
+++ src/visualizations.py
@@ -8,3 +8,16 @@ alt.data_transformers.disable_max_rows()
 scatterplot = alt.Chart(listing_data).mark_circle().encode(
     x=alt.X('review_scores_rating:Q', title='Review Ratings'),
     y=alt.Y('price:Q', title='Listing Prices')).properties(title='Price vs. Ratings', width=500, height=400)
+# scatterplot
+
+## geospatial heatmap - 
+import pandas as pd
+import folium
+from folium.plugins import HeatMap
+geospatial = listing_data[['latitude', 'longitude', 'price']].dropna()
+# base map centered around the mean latitude and longitude
+hm = folium.Map(location=[geospatial['latitude'].mean(), geospatial['longitude'].mean()], zoom_start=12)
+# map layer
+heat_data = list(zip(geospatial['latitude'], geospatial['longitude'], geospatial['price']))
+HeatMap(heat_data, radius=10, blur=15, max_zoom=1).add_to(hm)
+# hm
